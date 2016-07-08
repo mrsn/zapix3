b195d357ef9ad202cb660a410e5ecf762023fd34
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2016-07-08 17:53:34 +0200

    conventions ftw!

--- lib/zapix/proxies/proxies.rb
+++ lib/zapix/proxies/proxies.rb
@@ -2,9 +2,9 @@ require_relative 'base'
 
 class Proxies < Base
 
-  def get_id(options)
+  def get_id(name)
     result = client.proxy_get({
-      'filter' => {'name' => options['name']}})
+      'filter' => {'name' => name}})
       result.first['proxyid']
   end
 
