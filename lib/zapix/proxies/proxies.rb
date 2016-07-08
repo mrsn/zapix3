9e3eef190750666a498b7de44bf87bf2c5f5ea01
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2016-07-08 17:20:47 +0200

    fixed method name

--- lib/zapix/proxies/proxies.rb
+++ lib/zapix/proxies/proxies.rb
@@ -3,7 +3,7 @@ require_relative 'base'
 class Proxies < Base
 
   def get_id(options)
-    result = client.proxies_get({
+    result = client.proxy_get({
       'filter' => {'name' => options['name']}})
       result.first['proxyid']
   end
