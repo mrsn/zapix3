380cec10e2d6c72330dba92e0441b8d5193018cb
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2016-07-19 13:30:14 +0200

    removed activerecord validation

--- lib/zapix/zabbix_classes/interface.rb
+++ lib/zapix/zabbix_classes/interface.rb
@@ -1,18 +1,9 @@
-require 'active_record'
-
 class Interface
-  include ActiveModel::Validations
-
   attr_reader :type, :main, :useip, :ip, :dns, :port
-  validates_presence_of :type, :main, :useip, :ip, :dns, :port
 
   # for more info see
   #  https://www.zabbix.com/documentation/2.0/manual/appendix/api/hostinterface/definitions#host_interface
   # we assume ip and dns shall always be set
-
-  validates_inclusion_of :type, in: 1..4
-  validates_inclusion_of :main, in: 0..1
-  validates_inclusion_of :useip, in: 0..1
 
   def initialize(attributes)
     @type = attributes['type'] ||= 1
@@ -29,10 +20,6 @@ class Interface
       'dns' => dns,
       'port' => port
     }
-  end
-
-  def read_attribute_for_validation(key)
-    send(key)
   end
 
   def to_hash
